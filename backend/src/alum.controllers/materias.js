c732283fe5bcda6f9132772ba14e7feebd746905
juancjr <juancarlo14071@gmail.com>
2021-09-26 16:52:17 -0300

    update

--- backend/src/alum.controllers/materias.js
+++ backend/src/alum.controllers/materias.js
@@ -5,54 +5,69 @@ const pool = require("../database");
 
 //Get a todos los datos
 const GetALLM = router.get("/materias", (req, res) => {
-    pool.query("SELECT * FROM materias", (err, rows, fields) => {
-        if (!err) {
-            res.json(rows);
-        } else {
-            console.log(err)
-        }
-    })
+  pool.query("SELECT * FROM materias", (err, rows, fields) => {
+    if (!err) {
+      res.json(rows);
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 //Get por ID
 const GetOneM = router.get("/materias/:id", async (req, res) => {
-    const { id } = req.params;
-    console.log(id);
-    await pool.query("SELECT * FROM materias WHERE materia_id = ?", [id], (err, rows, fields) => {
-        if (!err) {
-            res.json(rows[0]);
-        } else {
-            console.log(err);
-        }
-    })
+  const { id } = req.params;
+  console.log(id);
+  await pool.query(
+    "SELECT * FROM materias WHERE materia_id = ?",
+    [id],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json(rows[0]);
+      } else {
+        console.log(err);
+      }
+    }
+  );
 });
 
-
-//Post insert datos 
+//Post insert datos
 const PostM = router.post("/materias", async (req, res) => {
-    const { materia } = req.body;
-    const newM = { materia };
-    await pool.query("INSERT INTO materias set ?", [newM], (err, rows, fields) => {
-        if (!err) {
-            res.json("Data save");
-        } else {
-            console.log(err)
-        }
-    });
-    res.send("recibido")
-})
+  const { materia, grado_id } = req.body;
+  
+  const newMateria = {
+    materia,
+    grado_id,
+  };
+
+  await pool.query(
+    "INSERT INTO materias set ?",
+    [newMateria],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json("Data save");
+      } else {
+        console.log(err);
+      }
+    }
+  );
+  res.send("recibido");
+});
 
 //Delete ID
 const DeleteM = router.delete("/materias/:id", async (req, res) => {
-    const { id } = req.params;
-    pool.query("DELETE FROM materias WHERE materia_id =?", [id], (err, rows, fields) => {
-        if (!err) {
-            res.json({ Materia: "Eliminado" });
+  const { id } = req.params;
+  pool.query(
+    "DELETE FROM materias WHERE materia_id =?",
+    [id],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({ Materia: "Eliminado" });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
 
-        } else {
-            console.log(err);
-        }
-    })
-})
-
-module.exports = { GetOneM, GetALLM, PostM, DeleteM }
+module.exports = { GetOneM, GetALLM, PostM, DeleteM };
